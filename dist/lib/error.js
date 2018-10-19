91dc83e4c5eb818bfff335bbdbf10a9265e6874e
JacopoPatrocloMartinelli <jacopo.martinelli@farnedi.it>
2018-10-19 11:01:27 +0200

    remove dist and update gitignore

--- dist/lib/error.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ErrorManager {
-    constructor(payload, type) {
-        this.payload = payload;
-        this.type = type;
-    }
-}
-exports.default = ErrorManager;
